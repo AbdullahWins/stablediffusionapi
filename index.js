7eb82234f71a140ef6e8ab1a0fb96413f688b131
Abdullah Al MahMud <abdudevs@gmail.com>
2023-07-13 13:13:36 +0600

    added some modifications

--- index.js
+++ index.js
@@ -1,129 +1,47 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const OAuth = require("oauth-1.0a");
-const crypto = require("crypto");
 const axios = require("axios");
-const { getToken, Client } = require("@threadsjs/threads.js");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// openai imports
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-// const oauth = OAuth({
-//   consumer: {
-//     key: process.env.TWITTER_API_KEY,
-//     secret: process.env.TWITTER_API_SECRET,
-//   },
-//   signature_method: "HMAC-SHA1",
-//   hash_function: (baseString, key) => {
-//     return crypto.createHmac("sha1", key).update(baseString).digest("base64");
-//   },
-// });
-
-// app.get("/api/twitter/login", (req, res) => {
-//   const request_data = {
-//     url: "https://api.twitter.com/oauth/request_token",
-//     method: "POST",
-//     data: { oauth_callback: "http://localhost:5000/callback" },
-//   };
-
-//   axios
-//     .request({
-//       url: request_data.url,
-//       method: request_data.method,
-//       headers: oauth.toHeader(oauth.authorize(request_data)),
-//       data: request_data.data,
-//     })
-//     .then((response) => {
-//       const { oauth_token, oauth_token_secret } = oauth.deParam(response.data);
-//       const redirectUrl = `https://api.twitter.com/oauth/authenticate?oauth_token=${oauth_token}`;
-//       res.redirect(redirectUrl); // Redirect the client to Twitter authentication
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//       res.status(500).send("Error occurred during Twitter login");
-//     });
-// });
-
-// app.get("/threads/:userId", async (req, res) => {
-//   const { userId } = req.params;
-
-//   try {
-//     const token = await getToken(process.env.un, process.env.ps);
-//     const client = new Client({ token });
-
-//     const user = await client.users.fetch("25025320");
-//     console.log(user);
-//     res.json(user);
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.status(500).send("Error occurred while fetching the user");
-//   }
-// });
-
-// app.get("/callback", (req, res) => {
-//   const { oauth_token, oauth_verifier } = req.query;
-
-//   const request_data = {
-//     url: "https://api.twitter.com/oauth/access_token",
-//     method: "POST",
-//     data: { oauth_token, oauth_verifier },
-//   };
-
-//   axios
-//     .request({
-//       url: request_data.url,
-//       method: request_data.method,
-//       headers: oauth.toHeader(oauth.authorize(request_data)),
-//       data: request_data.data,
-//     })
-//     .then((response) => {
-//       const { oauth_token, oauth_token_secret } = oauth.deParam(response.data);
-//       res.json({ accessToken: oauth_token });
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//       res.status(500).send("Error occurred during Twitter authentication");
-//     });
-// });
-
-// // openai
-
-// app.post("/openai", async (req, res) => {
-//   try {
-//     const data = req.body;
-//     console.log(data);
-
-//     const chatCompletion = await openai.createChatCompletion({
-//       model: "gpt-3.5-turbo",
-//       messages: [
-//         { role: "system", content: "You are a helpful assistant." },
-//         { role: "user", content: data.message },
-//       ],
-//     });
-
-//     console.log(chatCompletion);
-
-//     res.send(chatCompletion.data);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: "An error occurred" });
-//   }
-// });
-
 // stable
 
 app.post("/stable", (req, res) => {
-  console.log(req.body); // Log the request body to debug
+  console.log(req?.body); // Log the request body to debug
+
+  const preDefinedData = {
+    key: process.env.STABLE_DIFFUSION_API_KEY,
+    model_id: "cartoonish",
+    prompt:
+      "actual 8K full body image of a japanese naked teenage girl, with big boobs, detailed face, detailed boobs, detailed belly, detailed legs, happy colors, bright eyes, clear eyes, warm smile, smooth soft  skin, big dreamy eyes, beautiful intricate colored hair, symmetrical, anime wide eyes, soft lighting, detailed face, by makoto   shinkai, stanley artgerm lau, wlop, rossdraws, concept art, digital painting, realistic face, realistic boobs, realistic belly, realistic legs, realistic full body, teen girl looking into camera",
+    negative_prompt:
+      "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad  anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers,   missing lips, ugly face, distorted face, extra legs, anime",
+    width: "512",
+    height: "512",
+    samples: "1",
+    num_inference_steps: "30",
+    safety_checker: "no",
+    enhance_prompt: "yes",
+    seed: null,
+    guidance_scale: 7.5,
+    multi_lingual: "no",
+    panorama: "no",
+    self_attention: "no",
+    upscale: "no",
+    embeddings_model: null,
+    lora_model: null,
+    tomesd: "yes",
+    use_karras_sigmas: "yes",
+    vae: null,
+    lora_strength: null,
+    scheduler: "UniPCMultistepScheduler",
+    webhook: null,
+    track_id: null,
+  };
 
   const options = {
     method: "POST",
@@ -131,7 +49,7 @@ app.post("/stable", (req, res) => {
     headers: {
       "Content-Type": "application/json",
     },
-    data: req.body,
+    data: preDefinedData,
   };
 
   axios(options)
